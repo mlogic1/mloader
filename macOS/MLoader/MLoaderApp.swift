ffbe84f670afd276bc08f1a20318c36c6540288d
mlogic1 <multilogic@outlook.com>
2025-03-03 21:22:19 +0100

    basic xcode project setup

---
+++ macOS/MLoader/MLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLoaderApp.swift
+//  MLoader
+//
+//  Created by Filip Radic on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
